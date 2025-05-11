29a77ee3595e2f0c528fe3e5635a994f997e66b1
ibnehussain <63780759+ibnehussain@users.noreply.github.com>
2025-05-11 14:38:34 +0000

    Update home-page.service.ts

--- src/app/home/HomePage-Services/home-page.service.ts
+++ src/app/home/HomePage-Services/home-page.service.ts
@@ -20,7 +20,7 @@ export class HomePageService {
 
   //Getting the Products from backend API
   getProducts():Observable<IProduct[]>{
-    let tempVar = this.http.get<IProduct[]>('https://quickkart-backend.azurewebsites.net/api/home/getproducts')
+    let tempVar = this.http.get<IProduct[]>('https://apimtest012549.azure-api.net/api/home/getproducts')
     console.log(tempVar)
     return tempVar
   }
@@ -40,7 +40,7 @@ export class HomePageService {
     var user:User
     user={emailID:userEmailID, password:userPassword,usertype:type};
     console.log(user)
-    let result=this.http.post<number>('https://quickkart-backend.azurewebsites.net/api/LoginFunction?code=kDxJUURhVektI3q0G7_k0zea7O9yKG0FG-VfWfLqbLzpAzFuZJP6ww==',user)
+    let result=this.http.post<number>('https://funcazappnew.azurewebsites.net/api/LoginFunction?code=_SdjNEQqZYKW6rDEgIymYqw2WUx-oySU25RJVFn5WgGjAzFueuFDZQ==',user)
     return result
 
   }
